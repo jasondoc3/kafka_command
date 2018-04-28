ba9924778ec156e28d202c67b88f7dadcd37732e
jasondoc3 <jasondoc3@gmail.com>
2018-04-27 22:07:22 -0700

    Rename to KafkaCommand

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "kafkacommand_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "kafka_command_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
