158b67f4e568d85ce8101cb1157b37f3c449b96a
jasondoc3 <jasondoc3@gmail.com>
2018-05-01 23:19:50 -0700

    Add Kafka::BrokerWrapper spec

--- app/wrappers/kafka/broker_wrapper.rb
+++ app/wrappers/kafka/broker_wrapper.rb
@@ -3,6 +3,7 @@ require 'forwardable'
 module Kafka
   class BrokerWrapper
     extend Forwardable
+    attr_reader :broker
     def_delegators :@broker, :port, :host, :node_id, :fetch_metadata
 
     def initialize(broker)
---
+++ spec/wrappers/kafka/broker_wrapper_spec.rb
@@ -0,0 +1,64 @@
+require 'support/kafka_helper'
+require 'app/wrappers/kafka/client_wrapper'
+
+RSpec.describe Kafka::BrokerWrapper do
+  let(:broker) do
+    Kafka::ClientWrapper
+      .new(brokers: ['localhost:9092'])
+      .cluster
+      .brokers
+      .sample
+  end
+
+  describe '#new' do
+    it 'wraps a Kafka::Broker' do
+      expect(broker.broker).to be_an_instance_of(Kafka::Broker)
+    end
+  end
+
+  context 'forwarding' do
+    describe '#port' do
+      it 'forwards port to the Kafka::Broker' do
+        expect_any_instance_of(Kafka::Broker).to receive(:port)
+        broker.port
+      end
+
+      it 'returns the port' do
+        expect(broker.port).to eq(9092)
+      end
+    end
+
+    describe '#host' do
+      it 'forwards host to the Kafka::Broker' do
+        expect(broker.broker).to receive(:host)
+        broker.host
+      end
+
+      it 'returns the host' do
+        expect(broker.host).to eq('localhost')
+      end
+    end
+
+    describe '#node_id' do
+      it 'forwards node_id to the Kafka::Broker' do
+        expect(broker.broker).to receive(:node_id)
+        broker.node_id
+      end
+
+      it 'returns the node_id' do
+        expect(broker.node_id).to eq(broker.broker.node_id)
+      end
+    end
+
+    describe '#fetch_metadata' do
+      it 'forwards fetch_metdata to the Kafka::Broker' do
+        expect(broker.broker).to receive(:fetch_metadata).and_call_original
+        broker.fetch_metadata
+      end
+
+      it 'returns an instance of Kafka::Protocol::MetadataRequest' do
+        expect(broker.fetch_metadata).to be_an_instance_of(Kafka::Protocol::MetadataResponse)
+      end
+    end
+  end
+end
