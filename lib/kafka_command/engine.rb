94a120879fcd2210fa8dea8d2c079a1014f04e49
jasondoc3 <jasondoc3@gmail.com>
2018-11-23 16:57:35 -0800

    Fix asset precompilation

--- lib/kafka_command/engine.rb
+++ lib/kafka_command/engine.rb
@@ -1,5 +1,9 @@
 module KafkaCommand
   class Engine < ::Rails::Engine
     isolate_namespace KafkaCommand
+
+    initializer "kafka_command.assets.precompile" do |app|
+      app.config.assets.precompile += %w( kafka_command/* )
+    end
   end
 end
