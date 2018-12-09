a0d4f1dd4888e42ddf8e09f04d138e3f76208a91
jasondoc3 <jasondoc3@gmail.com>
2018-12-08 23:44:46 -0800

    Move partition wrapper spec to partition spec

--- spec/models/kafka_command/partition_spec.rb
+++ spec/models/kafka_command/partition_spec.rb
@@ -1,15 +1,10 @@
-require 'app/wrappers/kafka_command/client_wrapper'
+require 'app/models/kafka_command/cluster'
+require 'app/models/kafka_command/partition'
 
-RSpec.describe KafkaCommand::PartitionWrapper do
+RSpec.describe KafkaCommand::Partition do
   let(:topic_name) { "test-#{SecureRandom.hex(12)}" }
-  let(:partition) do
-    KafkaCommand::ClientWrapper
-      .new(brokers: ['localhost:9092'])
-      .cluster
-      .topics
-      .find { |t| t.name == topic_name }
-      .partitions
-      .sample
+  subject do
+    KafkaCommand::Cluster.all.first.topics.find { |t| t.name == topic_name }.partitions.sample
   end
 
   before { create_topic(topic_name) }
@@ -17,7 +12,7 @@ RSpec.describe KafkaCommand::PartitionWrapper do
 
   describe '#new' do
     it 'contains a reference to a topic wrapper' do
-      expect(partition.topic).to be_an_instance_of(KafkaCommand::TopicWrapper)
+      expect(subject.topic).to be_an_instance_of(KafkaCommand::Topic)
     end
   end
 
@@ -29,73 +24,73 @@ RSpec.describe KafkaCommand::PartitionWrapper do
     end
 
     it 'retrieves the latest offset' do
-      expect(partition.highwater_mark_offset).to eq(num_messages)
+      expect(subject.highwater_mark_offset).to eq(num_messages)
     end
   end
 
   describe '#as_json' do
     let(:expected_json) do
       {
-        isr: partition.isr,
-        leader: partition.leader,
-        id: partition.partition_id,
-        highwater_mark_offset: partition.highwater_mark_offset
+        isr: subject.isr,
+        leader: subject.leader,
+        id: subject.partition_id,
+        highwater_mark_offset: subject.highwater_mark_offset
       }.to_json
     end
 
     it 'equals the expected json' do
-      expect(partition.as_json.to_json).to eq(expected_json)
+      expect(subject.as_json.to_json).to eq(expected_json)
     end
   end
 
   context 'forwarding' do
     let(:partition_metadata) do
-      partition.instance_variable_get(:@partition_metadata)
+      subject.instance_variable_get(:@partition_metadata)
     end
 
     describe '#isr' do
       it 'forwards isr to the partition metadata' do
         expect(partition_metadata).to receive(:isr)
-        partition.isr
+        subject.isr
       end
 
       it 'returns the isr' do
-        expect(partition.isr).to be_an_instance_of(Array)
-        expect(partition.isr.first).to be_an_instance_of(Integer)
+        expect(subject.isr).to be_an_instance_of(Array)
+        expect(subject.isr.first).to be_an_instance_of(Integer)
       end
     end
 
     describe '#leader' do
       it 'forwards leader to the partition metadata' do
         expect(partition_metadata).to receive(:leader)
-        partition.leader
+        subject.leader
       end
 
       it 'returns the leader broker' do
-        expect(partition.leader).to be_an_instance_of(Integer)
+        expect(subject.leader).to be_an_instance_of(Integer)
       end
     end
 
     describe '#partition_id' do
       it 'forwards leader to the partition metadata' do
         expect(partition_metadata).to receive(:leader)
-        partition.leader
+        subject.leader
       end
 
       it 'returns the partition id' do
-        expect(partition.partition_id).to be > -1
+        expect(subject.partition_id).to be > -1
       end
     end
 
     describe '#replicas' do
       it 'forwards replicas to the partition metadata' do
         expect(partition_metadata).to receive(:replicas)
-        partition.replicas
+        subject.replicas
       end
 
       it 'returns the replica node ids' do
-        expect(partition.replicas).to be_an_instance_of(Array)
-        expect(partition.replicas.first).to be_an_instance_of(Integer)
+        expect(subject.replicas).to be_an_instance_of(Array)
+        expect(subject.replicas.first).to be_an_instance_of(Integer)
       end
     end
   end
